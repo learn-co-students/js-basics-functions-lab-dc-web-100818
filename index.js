7cd6e7b67ba0f64fc4f665a464d80650e3f76973
bethwilson07 <beth.ny.wlsn@gmail.com>
2018-11-18 12:49:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (street_num) {
+  const Hq = 42
+  return Math.abs(Hq - street_num)
+};
+
+function distanceFromHqInFeet (street_num) {
+  blocks = distanceFromHqInBlocks(street_num)
+  return blocks * 264
+};
+
+function distanceTravelledInFeet (start, end) {
+  blocks = Math.abs(start - end)
+  return blocks * 264
+};
+
+function calculatesFarePrice (start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return ((distance - 400) * 0.02);
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    };
+};
